a9e17771c832fcb0e4f64d077ac3a4a0d56296ca
Heavenly <wofkwork9@gmail.com>
2026-01-31 21:14:30 +0100

    Update repository URL for file downloads

--- main.lua
+++ main.lua
@@ -31,7 +31,7 @@ local playersService = cloneref(game:GetService('Players'))
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/'..readfile('HeavenlyVape/profiles/commit.txt')..'/'..select(1, path:gsub('HeavenlyVape/', '')), true)
+			return game:HttpGet('https://raw.githubusercontent.com/HeavenlyScripts/VapeV4ForRoblox/'..readfile('HeavenlyVape/profiles/commit.txt')..'/'..select(1, path:gsub('HeavenlyVape/', '')), true)
 		end)
 		if not suc or res == '404: Not Found' then
 			error(res)
@@ -63,7 +63,7 @@ local function finishLoading()
 				if shared.VapeDeveloper then
 					loadstring(readfile('HeavenlyVape/loader.lua'), 'loader')()
 				else
-					loadstring(game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/'..readfile('HeavenlyVape/profiles/commit.txt')..'/loader.lua', true), 'loader')()
+					loadstring(game:HttpGet('https://raw.githubusercontent.com/HeavenlyScripts/VapeV4ForRoblox/'..readfile('HeavenlyVape/profiles/commit.txt')..'/loader.lua', true), 'loader')()
 				end
 			]]
 			if shared.VapeDeveloper then
@@ -103,7 +103,7 @@ if not shared.VapeIndependent then
 	else
 		if not shared.VapeDeveloper then
 			local suc, res = pcall(function()
-				return game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/'..readfile('HeavenlyVape/profiles/commit.txt')..'/games/'..game.PlaceId..'.lua', true)
+				return game:HttpGet('https://raw.githubusercontent.com/HeavenlyScripts/VapeV4ForRoblox/'..readfile('HeavenlyVape/profiles/commit.txt')..'/games/'..game.PlaceId..'.lua', true)
 			end)
 			if suc and res ~= '404: Not Found' then
 				loadstring(downloadFile('HeavenlyVape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
